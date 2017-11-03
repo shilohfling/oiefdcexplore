2e2157a30b8eb503e3c220754a9b9bc1bdc507c1
Data Student <datastudent@fdc-production-3.dhcp.webster.edu>
2017-11-03 16:05:01 -0500

    Removed unique() and found a way to test only a couple pieces of data at a time

--- 01-GetCleanData.R
+++ 01-GetCleanData.R
@@ -1,11 +1,18 @@
 library(openxlsx)
 library(mscstexta4r)
 
+##Call the API by logging in with the URL and Key
+##Values passed here from file .mscskeys.json
 textaInit()
+
+##Create a blank data frame to put scrubbed data into
 data_clean <- as.data.frame(matrix(1, nrow(data_raw)))
+
+##Load raw data from excel file
 data_raw <- read.xlsx("../sources/RM_Outcomes Survey Data_2-20-2017_Working Response - macro.xlsm", 
                      "Response Data_Working")
-data_clean$comment.overall <- as.character(data_raw[,168])
-df <- as.data.frame(data_clean$comment.overall)
 
-data_clean$comment.overall <- WUTIL$getSentiment(df, key = NULL, max = 100)
+##Make a new column in the "clean" data frame that holds the qualitative data
+data_clean$comment.overall <- as.character(data_raw[,168])
+
+results <- textaSentiment(data_clean$comment.overall[16:17])
