bdb8e0b5169175057ebfe5df09b87e2c79c25f3f
shilohbradley <shilohbradley@yahoo.com>
2017-12-06 09:48:16 -0600

    Started plotting the data for likert questions

---
+++ 05-NumericalAnalysis.R
@@ -0,0 +1,43 @@
+##An R Script for plotting the numerical data
+library(openxlsx)
+library(dplyr)
+library(ggplot2)
+library(likert)
+
+##Don't need this part - can be found in 01-GetCleanData.R
+#data_raw <- read.xlsx("../sources/RM_Outcomes Survey Data_2-20-2017_Working Response - macro.xlsm", 
+#                      "Response Data_Working")
+#NumAnalysis1 <- data.frame("id.student" = data_raw$`Student.ID.(Complete)`)
+
+##Data frame for first section of questions
+NumAnalysis1$admissions <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.a..Applying.to.and.being.admitted.by.Webster`
+NumAnalysis1$orientation <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.b..Initial.orientation.to.Webster.and.my.program.of.study`
+NumAnalysis1$academic.advising <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.c..Academic.advising`
+NumAnalysis1$registering.for.classes <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.d..Registering.for.classes`
+NumAnalysis1$financial.aid <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.e..Applying.for.and.receiving.financial.aid`
+NumAnalysis1$paying.tuition <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.f..Paying.tuition.and.fees`
+NumAnalysis1$library.resources <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.g..Accessing.and.using.library.resources`
+NumAnalysis1$technology <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.h..Using.Webster.technology.to.assist.in.my.learning`
+NumAnalysis1$writing.center <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.i..Accessing.and.using.the.Writing.Center`
+NumAnalysis1$career.planning <- data_raw$`Please.rate.your.level.of.satisfaction.with.the.following.Webster.University.processes..-.j..Assistance.with.career.planning`
+
+
+#####EXPLORATION FOR PLOTTING THE DATA#####
+#dataRange = as.data.frame(c(0:5))
+section1 <- ggplot(NumAnalysis1, aes(x=admissions)) + geom_bar()
+section1
+section1q <- hist(NumAnalysis1$admissions)
+section1q
+##Try likert package
+?likert
+?plot.likert
+section1a <- likert.bar.plot(NumAnalysis1, admissions)
+section1b <- likert.density.plot(NumAnalysis1, facet = T, bw = 0.5)
+?likert.density.plot
+
+
+
+
+
+
+
