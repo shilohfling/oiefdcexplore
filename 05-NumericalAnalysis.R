5841b00ac8f5b39bbecdbe1738627dcdc2de5e5b
shilohbradley <shilohbradley@yahoo.com>
2017-12-08 09:50:58 -0600

    Working on scaling the data and plotting likert scale data

--- 05-NumericalAnalysis.R
+++ 05-NumericalAnalysis.R
@@ -23,21 +23,42 @@ NumAnalysis1$career.planning <- data_raw$`Please.rate.your.level.of.satisfaction
 
 
 #####EXPLORATION FOR PLOTTING THE DATA#####
-#dataRange = as.data.frame(c(0:5))
-section1 <- ggplot(NumAnalysis1, aes(x=admissions)) + geom_bar()
-section1
-section1q <- hist(NumAnalysis1$admissions)
-section1q
-##Try likert package
-?likert
-?plot.likert
-section1a <- likert.bar.plot(NumAnalysis1, admissions)
-section1b <- likert.density.plot(NumAnalysis1, facet = T, bw = 0.5)
-?likert.density.plot
+# #dataRange = as.data.frame(c(0:5))
+# section1 <- ggplot(NumAnalysis1, aes(x=admissions)) + geom_bar()
+# section1
+# section1q <- hist(NumAnalysis1$admissions)
+# section1q
+# ##Try likert package
+# ?likert
+# ?plot.likert
+# section1a <- likert.bar.plot(NumAnalysis1, admissions)
+# section1b <- likert.density.plot(NumAnalysis1, facet = T, bw = 0.5)
+# ?likert.density.plot
 
 
+# 
+# section1c <- ggplot(NumAnalysis1,
+#                     aes(x=admissions)) +
+#                 theme(legend.position = "top",
+#                       axis.text = element_text(size = 6))
+# (section1d <- sections1c + geom_point(aes(color = )))
+
+p5 <- ggplot(NumAnalysis1)
+p5 + geom_line(aes(color = ))
 
 
+min_rank(NumAnalysis1$admissions)
+NumAnalysis1a <- percent_rank(NumAnalysis1$admissions)
+
+p6 <- ggplot(NumAnalysis1a, aes(admissions)) + geom_point()
+
+mean(NumAnalysis1a)
+
+##Need to figure out the syntax that will allow me to facet wrap the plot 
+##Then all the plots will show up in one big plot of individual plots
+##http://tutorials.iq.harvard.edu/R/Rgraphics/Rgraphics.html#faceting_to_the_rescue
 
 
-
+p7 <- ggplot(NumAnalysis1)
+p7 + geom_bar()
+p7
