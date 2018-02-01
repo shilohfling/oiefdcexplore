c620dc4ccaf75ef63a3af8ce12d236cfc15d5b8f
Erik Palmore <erikpal@gmail.com>
2018-02-01 13:07:12 -0600

    Added: local sources folder for data for better portability, select all to campuses, question selection, a new plot, converted Qs to factors, and added a new plot.

--- SubsetExportTool/server.R
+++ SubsetExportTool/server.R
@@ -4,6 +4,8 @@ library(DT)
 library(ggplot2)
 library(dplyr)
 library(purrr)
+library(sjPlot)
+
 
 ##Create a character vector of the columns to make available for export
 cols <- c("Campus" = "Campus.(not.scrubbed)", 
@@ -11,20 +13,23 @@ cols <- c("Campus" = "Campus.(not.scrubbed)",
           "Department" = "Department.(from.Recipients.and.Response.Rates.Data.Set)",
           "StudentID" = "Student.ID.(Complete)")
 
-questionsIndex <- read.csv("~/Data/oiefdcexplore/questionIndex.csv")
+questionsIndex <- read.csv("sources/questionIndex.csv")
+##questionsIndex <- read.csv("~/Data/oiefdcexplore/questionIndex.csv")
 
 ##Load the data object from disk
-dataraw <- readRDS("~/Data/oiefdcexplore/data.RDS")
+#dataraw <- readRDS("~/Data/oiefdcexplore/data.RDS")
+dataraw <- readRDS("sources/data.RDS")
 
 datanew <- dataraw[, cols]
 colnames(datanew) <- names(cols)
 
 data <- dataraw[, as.character(questionsIndex$Qualtrics)]
 colnames(data) <- questionsIndex$Question
+data <- lapply(data, as.factor)
 data <- cbind(datanew, data)
 
 ##Create subset choice vectors
-campus_choices <- unique(sort(data$Campus)) 
+campus_choices <- c(unique(sort(data$Campus)), "Select All")
 dept_choices <- unique(sort(data$Department))
 major_choices <- unique(sort(data$Major))
 
@@ -32,10 +37,20 @@ major_choices <- unique(sort(data$Major))
 question_choices <- questionsIndex %>% split(questionsIndex$Category) %>%
         map(~ as.character(.x$Shortname))
 
-shinyServer(function(input, output) {
+#Make a named vector to correlate input options to column name
+question_shortname_column_index <- questionsIndex$Question
+names(question_shortname_column_index) <- questionsIndex$Shortname
+
+shinyServer(function(input, output, session) {
 
         datasetInput <- reactive({
                 DTX <- data
+                
+                ##Subsetting
+                if("Select All" %in% input$campus) {
+                      selected_campus_choices <- setdiff(campus_choices, "Select All")
+                      updateSelectInput(session, "campus", selected = selected_campus_choices)
+                }
                 
                 DTX <- DTX[DTX$Campus %in% input$campus, ]
                 
@@ -47,13 +62,27 @@ shinyServer(function(input, output) {
                         DTX <- DTX[DTX$Major %in% input$major, ]   
                 }
                 
+                ##Exract the questions and join back the ones select
+                DTXquestions <- DTX %>% select(starts_with("Q"))
+                DTX <- DTX %>% select(-starts_with("Q"))
+                
+                DTXquestions <- DTXquestions[, question_shortname_column_index[input$questions]]
+                
+                ##If the length is one, the vector is returned and the name of the object is the colname
+                ##This isn't elegant, but lets us control the colname of a single question
+                if(length(input$questions) == 1) {
+                      DTX <- cbind(DTX, "Q" = DTXquestions)
+                } else {
+                      DTX <- cbind(DTX, DTXquestions)
+                }
+                
                 DTX
         })
         
         output$mainbody <- renderUI({
                 fluidPage(
-                        theme = shinytheme("yeti"),
                         #shinythemes::themeSelector(),
+                        theme = shinytheme("yeti"),
                         titlePanel("Outcomes Subset Export Tool"),
                         
                         sidebarLayout(
@@ -98,9 +127,9 @@ shinyServer(function(input, output) {
         })
         
         output$plot <- renderPlot({
-                ## Want to facet wrap this by selected questions
-                ## Currently just facet wrap by campus for demonstration
-                ggplot(datasetInput(), aes(`33`, fill = Department)) + geom_histogram() + xlim(0.5,4.5) + facet_wrap(~Campus)
+              if(nrow(datasetInput()) > 1 && length(input$questions) >= 1) {
+                    sjp.likert(select(datasetInput(), starts_with("Q")))
+              }
         })
         
         output$value <- renderPrint({
