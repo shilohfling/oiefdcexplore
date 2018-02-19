07fedac2be5f0b38bb6a45afdab2f7505c2a944c
shilohbradley <shilohbradley@yahoo.com>
2018-02-19 15:37:14 -0600

    Almost done with making Table 8.

--- SubsetExportTool/functions.R
+++ SubsetExportTool/functions.R
@@ -66,14 +66,33 @@ Table7 <- function(x, questions) {
        return(z)
               
 }
-z <- Table7(data, paste0("Q", 33:42))
+#z <- Table7(data, paste0("Q", 33:42))
 
 Table8 <- function(x, questions) {
-        x %>% levelQuestionAvgFreq(questions) %>% 
-                add_count(Q) %>% 
-                rename(F.Respondents.in.Avg = n) %>% 
-                group_by(Q, X.Program.Level, F.Program.Level.Avg, F.Respondents.in.Avg) %>% 
-                summarise() %>% ungroup() %>%
-                spread(Q, X.Program.Level, F.Program.Level.Avg, F.Respondents.in.Avg)
+      x <- x %>% levelQuestionAvgFreq(questions) %>%
+        add_count(Q) %>%
+        rename(F.Respondents.in.Avg = n) %>%
+        group_by(Q, X.Program.Level) %>% 
+        add_count(Q) %>%
+        add_count(X.Program.Level) %>%
+        rename(F.Total.Respondents = n) %>%
+        mutate(F.Overall.Percentage = F.Total.Respondents) %>%
+        rename(F.Program.Respondents.in.Avg = n) %>%
+        group_by(Q, F.Overall.Percentage, F.Respondents.in.Avg, 
+                 X.Program.Level, F.Program.Level.Avg, F.Program.Respondents.in.Avg) %>%
+        summarise() %>% ungroup() 
+      
+      y <- x %>% select(-F.Program.Level.Avg) %>% 
+        spread(X.Program.Level, F.Program.Respondents.in.Avg) %>% 
+        rename(F.GRAD.Program.Respondents.in.Avg = GRAD) %>% 
+        rename(F.UNDG.Program.Respondents.in.Avg = UNDG)
+      
+      z <- x %>% select(-F.Program.Respondents.in.Avg) %>% 
+        spread(X.Program.Level, F.Program.Level.Avg) %>% 
+        rename(F.GRAD.Program.Level.Avg = GRAD) %>% 
+        rename(F.UNDG.Program.Level.Avg = UNDG) %>% 
+        left_join(y, by = c("Q", "F.Respondents.in.Avg", "F.Overall.Percentage"))
+      
+  return(z)
 }
 
